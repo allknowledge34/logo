ede6e655f2d231387c7e49398f191a3b11d51df8
SachinKumar <166914290+allknowledge34@users.noreply.github.com>
2025-03-13 13:11:10 +0530

    Update route.jsx

--- app/api/users/route.jsx
+++ app/api/users/route.jsx
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+"use client"
 import { doc, getDoc,setDoc } from "firebase/firestore"
 import { db } from "@/config/FirebaseConfig"
 import { NextResponse } from "next/server";
