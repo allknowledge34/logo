d8ee3f802ad8b7edf846af273b88e0d80b027fdc
SachinKumar <166914290+allknowledge34@users.noreply.github.com>
2025-03-13 13:15:25 +0530

    Update Description.jsx

--- app/_components/Description.jsx
+++ app/_components/Description.jsx
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-"use client"
 import Image from 'next/image'
 import React from 'react'
 
