7dffa314b82a2c71fce9073278919e1a07438fe5
SachinKumar <166914290+allknowledge34@users.noreply.github.com>
2025-03-13 13:10:56 +0530

    Update route.jsx

--- app/api/ai-logo-model/route.jsx
+++ app/api/ai-logo-model/route.jsx
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+"use client"
 import { AILogoPrompt } from "@/config/AiModel"
 import { db } from "@/config/FirebaseConfig"
 import axios from "axios"
