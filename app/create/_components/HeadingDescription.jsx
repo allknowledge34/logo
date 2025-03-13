3e0239cdfc388041dcf669ad41f9ce98566b0645
SachinKumar <166914290+allknowledge34@users.noreply.github.com>
2025-03-13 13:04:58 +0530

    Update HeadingDescription.jsx

--- app/create/_components/HeadingDescription.jsx
+++ app/create/_components/HeadingDescription.jsx
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+"use client"
 import React from 'react'
 
 function HeadingDescription({title,description}) {
