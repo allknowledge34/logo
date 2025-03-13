a8e2e185c890db957e591f902eb165ba33a1172d
SachinKumar <166914290+allknowledge34@users.noreply.github.com>
2025-03-13 13:15:35 +0530

    Update Footer.jsx

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-"use client"
+
 import Image from 'next/image'
 import React from 'react'
 
